213b256e284123fe9d340a468397738caf71c9f4
yazzan-96 <yazanaraddadi@gmail.com>
2021-10-13 21:59:35 +0300

    added new function

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
-# Todolist
+                                                Time Management
+
+
+OverviewOverview :
+
+The project will help you to organize your tasks with the possibility of deleting or postponing them with the addition of arranging your tasks according to priority or whether you finish them or not.
+
+upcaming Features :
+
+- I Will Add More Tasks.
+
+- Arrang The Tasks By Date.
+
+- More Ease Of Use.
+
+- Creative Touches.
+
+The feature I liked the most :
+
+- arranging the tasks by priority.
+
--- Todolist.playground/Contents.swift
+++ Todolist.playground/Contents.swift
@@ -1,70 +1,106 @@
+enum Status : Int {
+    case Complete
+    case NotComplete
+}
+enum Importance: Int {
+    case high
+    case medium
+    case low
+}
+
 struct TaskItem {
     var title: String
     var description: String
-    var dateOfFinish: String
-    var time: String
-    var isComplete: Bool
+    var DayToFinish: String
+    var Priority : Importance
+    var isComplete: Status
     
 }
 
 class RemainderList {
     
-    var firstWeekTasks: [TaskItem]
+    var Tasks: [TaskItem]
+    
     
     init() {
-        firstWeekTasks = []
-    }
-    
-    func addItem(item: TaskItem) {
-        firstWeekTasks.append(item)
-    }
-    
-    func remove (index : Int){
-        firstWeekTasks.remove(at: index)
-    }
-    
-    func update (index : Int , element : String){
-        firstWeekTasks [index].dateOfFinish = element
+        Tasks = []
         
     }
-    func printitems (){
-        for i in firstWeekTasks {
-            print(i.self)
-        }}
+    
+    func addItem(item: TaskItem) {  // if you want to add task
+        Tasks.append(item)
+        
+    }
+    
+    func remove (index : Int){  // if you to remove task
+        Tasks.remove(at: index)
+    }
+    
+    func update (index : Int , Day : String){  //if you want to delay task
+        Tasks [index].DayToFinish = Day
+    }
+    
+    func printitems (){  // printing if the title is complete or not
+        
+        for i in Tasks {
+            print(i.title," ", i.DayToFinish ," " ,i.Priority," " ,i.isComplete)
+        }
+    }
+    
+    func sortdbypriority () {                     //sorting by priority
+        Tasks = Tasks.sorted { $0.Priority.rawValue < $1.Priority.rawValue }
+        print()
+    }
+    func sortdbyisComplete () {                     //sorting by priority
+        Tasks = Tasks.sorted { $0.isComplete.rawValue < $1.isComplete.rawValue }
+        print()
+    }
+    
+    
+    
+}
+
+func printAllListItems(lists: RemainderList...) { // printing all tasks (outside class)
+    
+    for list in lists {
+        print ("\n\n List Items....")
+        list.printitems()
+    }
 }
 
 
-var Task1 = TaskItem( title: "repiar", description: "fixing may car ", dateOfFinish: "friday ", time: "8:00", isComplete: true)
-var Task2 = TaskItem( title: "calling", description: "call my uncle", dateOfFinish: "today", time: "12:12", isComplete: false)
-var Task3 = TaskItem(title: "Meeting", description: "job interview" , dateOfFinish: "sunday", time: "10:00 am", isComplete: false)
+//personal list
+var Task1 = TaskItem(title: "visiting",description: "viist my freind",DayToFinish:"Thursday", Priority:.low, isComplete: .Complete)
+var Task2 = TaskItem( title:"Gift", description: "reward myself", DayToFinish: "friday",Priority:.high,isComplete: .NotComplete)
+var Task3 = TaskItem(title: "Travel",description: "refreshing", DayToFinish: "sunday", Priority:.medium, isComplete: .Complete)
 
 
-//print(day1.title)
-//print(day2.title)
+//Work List
+var Task4 = TaskItem(title:"Meeting",description:"job interview" ,DayToFinish: "Wednesday", Priority: .low, isComplete: .Complete)
+var Task5 = TaskItem(title:"project",description:"finishing my project",DayToFinish:"sunday",Priority:.high,isComplete: .NotComplete)
+var Task6 = TaskItem(title: "emails",description:"respond to emails",DayToFinish: "Tuesday", Priority:.medium,isComplete: .NotComplete)
 
-var list = RemainderList()
 
-list.addItem(item: Task1)
-list.addItem(item: Task2)
-list.addItem(item: Task3)
-list.addItem(item:TaskItem(title:"visiting", description: "vist my brother", dateOfFinish: "friday", time: "5:00", isComplete: false) )
-list.update(index: 3, element: "sdd")
-//print(list.firstWeekTasks[3])
-list.remove(index: 0)
 
-list.update(index: 1, element: "jj")
-//list.printitems()
-//print(Task1)
-////
-list.printitems()
+
+var PersonalList = RemainderList()
+PersonalList.addItem(item: Task1)
+PersonalList.addItem(item: Task2)
+PersonalList.addItem(item: Task3)
+//PersonalList.printitems()
 
 
 
 
 
+var WorkList = RemainderList()
+WorkList.addItem(item: Task4)
+WorkList.addItem(item: Task5)
+WorkList.addItem(item: Task6)
+WorkList.sortdbypriority()
+WorkList.printitems()
 
 
 
-
-
+printAllListItems(lists: PersonalList, WorkList)
 
